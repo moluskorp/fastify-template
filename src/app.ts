60ccb5d078721b8cdf20087afe0da3df44e2e605
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-02-22 08:31:28 -0300

    Fix routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(appRoutes)
